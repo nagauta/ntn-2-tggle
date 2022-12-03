723efbbd6a03b73c6af1260fa64000e7749b25a9
mie035 <Notakes035@gmail.com>
2022-12-04 02:42:13 +0900

    update entry point

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
 "use strict";
 require('dotenv').config();
+const {DateTime} = require("luxon")
 const NotionTaskAgent = require('./notionTaskAgent')
 const TogglAgent = require('./togglAgent')
 
 const ntnTskAgnt = new NotionTaskAgent();
 const togglAgent = new TogglAgent();
-async function main() {
+/**
+ * 
+ * @param {*} targetDate yyyy-MM-dd
+ */
+async function main(targetDate) {
+    
+    const tes = await togglAgent.getTimeEntries(targetDate);
+    await togglAgent.deleteTimeEntries(tes);
     syncProjectsToToggle();
-    syncAcheivementsToToggle();
+    syncAcheivementsToToggle(targetDate);
 }
 async function syncProjectsToToggle() {
     let ret = await ntnTskAgnt.getProjects();
     console.log(`${JSON.stringify(ret)}`);
     togglAgent.syncProjects(ret);
 }
-async function syncAcheivementsToToggle() {
-    const acievements = await ntnTskAgnt.getAchivementsToday();
+async function syncAcheivementsToToggle(targetDate) {
+    const acievements = await ntnTskAgnt.getAchivements(DateTime.now().toFormat(targetDate));
     console.log(`acievements ${acievements}`);
-    const updated = togglAgent.syncAchivements(acievements);
+    const updated = await togglAgent.syncAchivements(acievements);
     console.log(`updated ${updated}`);
 }
-main();
+
+if(process.argv[2] === undefined){
+    throw new Error("日付(yyyy-MM-dd)を指定するように");
+}
+main(process.argv[2]);
